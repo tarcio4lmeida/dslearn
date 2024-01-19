f85872e7c51e6482931e7c5d204258f4c961f436
tarcio almeida <tarcioalmeidalima@gmail.com>
2024-01-19 17:52:12 -0300

    validação, exceção e segurança

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -0,0 +1,43 @@
+package com.devsuperior.dslearnbds.services;
+
+import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private UserRepository repository;
+	
+//	@Autowired
+//	private AuthService authService;
+//
+//	@Transactional(readOnly = true)
+//	public UserDTO findById(Long id) {
+//		authService.validateSelfOrAdmin(id);
+//		Optional<User> obj = repository.findById(id);
+//		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+//		return new UserDTO(entity);
+//	}
+//
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("User found: " + username);
+		return user;
+	}
+}
